c58adab21aad92393184d78a8f4547239c2cbd00
Yashwant Sahoo <yashwantsahoo05@gmail.com>
2024-09-27 16:15:49 +0530

    mainchanges

--- lib/main.dart
+++ lib/main.dart
@@ -13,29 +13,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-<<<<<<< HEAD
         primarySwatch: Colors.blue,
-=======
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
->>>>>>> 80998ba65ddf67fd6d7b31f88d464aceb34c2a96
       ),
       home: const SplashScreen(),
     );
   }
 }
 
-<<<<<<< HEAD
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
-=======
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-
-  final String title;
->>>>>>> 80998ba65ddf67fd6d7b31f88d464aceb34c2a96
 
   @override
   _SplashScreenState createState() => _SplashScreenState();
@@ -44,7 +30,6 @@ class MyHomePage extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   double _opacity = 0.0;
 
-<<<<<<< HEAD
   @override
   void initState() {
     super.initState();
@@ -62,42 +47,20 @@ class _SplashScreenState extends State<SplashScreen> {
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (context) => const HomeScreen()),
       );
-=======
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
->>>>>>> 80998ba65ddf67fd6d7b31f88d464aceb34c2a96
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-<<<<<<< HEAD
       body: Center(
         child: AnimatedOpacity(
           opacity: _opacity,
           duration: const Duration(seconds: 5),
-          child: Image.asset('assets/images/bargraph.gif'),
-=======
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times 🤔🤔',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
->>>>>>> 80998ba65ddf67fd6d7b31f88d464aceb34c2a96
+          child: const Text(
+            'Welcome to the Home Screen!',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
         ),
       ),
     );
@@ -114,7 +77,10 @@ class HomeScreen extends StatelessWidget {
         title: const Text('Home Screen'),
       ),
       body: const Center(
-        child: Text('Welcome to the Home Screen!'),
+        child: Text(
+          'Welcome to the Home Screen!',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
       ),
     );
   }
